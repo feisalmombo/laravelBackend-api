e01cc18760c50f35ccfe8dc4a67719709ca98651
feisalmombo <feisalmombo29@gmail.com>
2024-04-23 14:30:55 +0300

    modify routes for api.php file

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// API routes
+Route::get("list-students", [ApiController::class, "listStudents"]);
+
+Route::get("single-student/{id}", [ApiController::class, "getSingleStudent"]);
+
+Route::post("add-student", [ApiController::class, "CreateStudent"]);
+
+Route::put("update-student/{id}", [ApiController::class, "updateStudent"]);
+
+Route::delete("delete-student/{id}", [ApiController::class, "deleteStudent"]);
