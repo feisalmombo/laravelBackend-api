9e16754c446c356157e6c13b9466630f4bcffc6c
feisalmombo <feisalmombo29@gmail.com>
2024-04-23 13:00:13 +0300

    modify students migration

--- database/migrations/2024_04_23_095419_create_students_table.php
+++ database/migrations/2024_04_23_095419_create_students_table.php
@@ -15,6 +15,11 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->increments('id');
+            $table->string("name",100);
+            $table->string("email",100);
+            $table->integer("age")->unsigned();
+            $table->string("phone_no",20);
+            $table->enum("gender",["male","female"]);
             $table->timestamps();
         });
     }
