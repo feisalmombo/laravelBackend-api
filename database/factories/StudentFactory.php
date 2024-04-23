064e4daf87cf92078064efd7803b932a8d400b2d
feisalmombo <feisalmombo29@gmail.com>
2024-04-23 13:23:06 +0300

    modify studentfactory file

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -1,9 +1,31 @@
 <?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Student::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
+namespace Database\Factories;
+use App\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+class StudentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Student::class;
+/**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            "name" => $this->faker->name(),
+            "email" => $this->faker->safeEmail,
+            "phone_no" => $this->faker->phoneNumber,
+            "age" => $this->faker->numberBetween(15, 45),
+            "gender" => $this->faker->randomElement([
+                "male",
+                "female"
+            ])
+        ];
+    }
+}
