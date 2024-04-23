e7f3342c4ea3905ea9287fe829a486c3122bf6c2
feisalmombo <feisalmombo29@gmail.com>
2024-04-23 14:17:44 +0300

    modify api controller

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -7,5 +7,33 @@ use App\Http\Controllers\Controller;
 
 class ApiController extends Controller
 {
-    //
+    //Create Student
+    public function CreateStudent(Request $request)
+    {
+
+    }
+
+    //List Students
+    public function listStudents()
+    {
+
+    }
+
+    // Single student API
+    public function getSingleStudent($id)
+    {
+
+    }
+
+    // Update Student
+    public function updateStudent(Request $reques , $id)
+    {
+
+    }
+
+    // Delte Student
+    public function deleteStudent($id)
+    {
+
+    }
 }
